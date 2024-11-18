be48eec0b61f146cabe205a1bd14a233ebfc9a8c
Philip Mayrhofer <cc241079@fhstp.ac.at>
2024-11-18 11:18:00 +0100

    feat: add click handler

--- game.js
+++ game.js
@@ -11,6 +11,17 @@ document.addEventListener('DOMContentLoaded', () => {
 function createGameCanvas() {
     const gameCanvas = document.getElementById('gameCanvas');
     const ctx = gameCanvas.getContext('2d');
+
+    // outline
     ctx.lineWidth = 12;
-    ctx.strokeRect(0, 0, ctx.width ?? 600, ctx.height ?? 600);
+    ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
+
+    gameCanvas.addEventListener('mouseup', (event) => {
+        const rect = gameCanvas.getBoundingClientRect();
+
+        const x = Math.round(event.clientX - rect.left);
+        const y = Math.round(event.clientY - rect.top);
+        ctx.fillStyle = 'green';
+        ctx.fillRect(x - 10, y - 10, 20, 20);
+    })
 }
